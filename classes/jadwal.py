c9261bfefcd8be60985760fb98e096b9008701c3
Asyary Raihan <asyaryraihan@gmail.com>
2024-10-28 23:14:11 +0700

    Bjir

--- classes/jadwal.py
+++ classes/jadwal.py
@@ -12,20 +12,12 @@ class Jadwal:
         
         # Initialize instance variables with data from the first result, if available
         for get_jadwal in self.jadwal_data:
-<<<<<<< HEAD
             first_jadwal = get_jadwal
             self.id_jadwal = first_jadwal.get('id_jadwal')
             self.harga_eko = first_jadwal.get('harga_eko')
             self.harga_bis = first_jadwal.get('harga_bis')
             self.harga_eks = first_jadwal.get('harga_eks')
             self.waktu = first_jadwal.get('waktu')
-=======
-            self.id_jadwal = get_jadwal.get('id')
-            self.harga_eko = get_jadwal.get('harga_eko')
-            self.harga_bis = get_jadwal.get('harga_bis')
-            self.harga_eks = get_jadwal.get('harga_eks')
-            self.waktu = get_jadwal.get('waktu')
->>>>>>> 707e75ee24973372d32cb04c3ab63d417764407d
         else:
             # Set to None if no data is found
             self.id_jadwal = None
@@ -37,22 +29,15 @@ class Jadwal:
     def valid(self):
         self.db.connect()
         query = """
-<<<<<<< HEAD
             SELECT j.*, s_awal.nama AS s_awal, s_akhir.nama AS s_akhir
             FROM jadwal AS j
             JOIN stasiun AS s_awal ON j.stasiun_awal = s_awal.id
             JOIN stasiun AS s_akhir ON j.stasiun_akhir = s_akhir.id
             WHERE j.stasiun_awal = ? AND j.stasiun_akhir = ?
-=======
-            SELECT id, stasiun_awal, stasiun_akhir, harga_eko, harga_bis, harga_eks, waktu 
-            FROM jadwal
-            WHERE stasiun_awal = ? AND stasiun_akhir = ?
->>>>>>> 707e75ee24973372d32cb04c3ab63d417764407d
         """
         self.db.cursor.execute(query, (self.stasiun_awal, self.stasiun_akhir))
         rows = self.db.cursor.fetchall()
-
-        # Convert rows to dictionaries assuming fetchall() returns rows with column names as keys
+        
         result = [dict(row) for row in rows] if rows else None
         self.db.close()
         return result
@@ -66,13 +51,8 @@ class Jadwal:
         for index, jadwal in enumerate(self.jadwal_data, start=1):
             print(f"Schedule {index}:")
             print(f"  ID Jadwal      : {jadwal.get('id')}")
-<<<<<<< HEAD
             print(f"  Stasiun Awal   : {jadwal.get('s_awal')}")
             print(f"  Stasiun Akhir  : {jadwal.get('s_akhir')}")
-=======
-            print(f"  Stasiun Awal   : {jadwal.get('stasiun_awal')}")
-            print(f"  Stasiun Akhir  : {jadwal.get('stasiun_akhir')}")
->>>>>>> 707e75ee24973372d32cb04c3ab63d417764407d
             print(f"  Harga Ekonomi  : {jadwal.get('harga_eko')}")
             print(f"  Harga Bisnis   : {jadwal.get('harga_bis')}")
             print(f"  Harga Eksekutif: {jadwal.get('harga_eks')}")
