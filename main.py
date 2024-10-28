c316ff3062ef6a592383e22b58be947746e38fa2
Asyary Raihan <asyaryraihan@gmail.com>
2024-10-28 21:33:45 +0700

    Register example

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
+from utils.gui import GUI
 from classes.user import User
 
 def init():
-	user = User('andisetiawan@example.com', 'admin123')
-	if user.id is None:
-		print("User not found")
-	else:
-		print("User found")
-		print(user.id)
+	user = User('aandisetiawan@example.com', 'admin1234', '1234123412341234', 'Aandi Setiawan')
+	print(user.status)
 
 if __name__ == "__main__":
     init()
