8945489158c9446614d23ed41671f7aadcedf479
MrRaffs <muhrafirafsanjani@gmail.com>
2024-10-28 20:45:15 +0700

    user data retrieve

--- classes/user.py
+++ classes/user.py
@@ -1,11 +1,32 @@
+import sqlite3
 class User:
-    def __init__(self, id_user, nik, nama, email, role = "user"):
-       self.id_user = id_user
-       self.nik = nik
-       self.nama = nama
-       self.email = email
-       self.role = role
+    def __init__(self, email, password):
+        self.email = email
+        self.password = password
+        self.db = DbModel.DbModel()
+        user_data = self.login()
+		if user_data:
+			self.id = user_data.get('id')
+			self.nik = user_data.get('nik')
+			self.nama = user_data.get('nama')
+			self.role = user_data.get('role')
+		else:
+			self.id = None
+			self.nik = None
+			self.nama = None
+			self.email = None
+			self.password = None
+			self.role = None
+            
+	def login(self):
+		self.db.connect()
+		query = "SELECT * FROM user WHERE email = ? AND password = ?"
+		self.db.cursor.execute(query, (self.email, self.password))
+		row = self.db.cursor.fetchone()
+		result = dict(row) if row else None
+		self.db.close()
+		return result
 
 class Admin (User):
-    def __init__(self, id_user, nik, nama, email, role='admin'):
-        super().__init__(id_user, nik, nama, email, role)
+    def __init__(self, email, password):
+        super().__init__(email, password, role='admin')
