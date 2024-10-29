55a284506a5029c91e12f141dcb6384861ae10cc
Asyary Raihan <asyaryraihan@gmail.com>
2024-10-29 13:07:10 +0700

    Add pencarian stasiun

--- utils/helper.py
+++ utils/helper.py
@@ -1,5 +1,6 @@
 from .model import DbModel
 from classes.user import User
+from classes.stasiun import Stasiun
 
 class Utils:
 	def cek_koneksi():
@@ -14,8 +15,20 @@ class Utils:
 
 	def login(email, password):
 		user = User(email, password)
+		if (user.status):
+			print("Logged in as", user.nama)
 		return user
 	
 	def register(email, password, nik, nama):
 		user = User(email, password, nik, nama)
+		if (user.status_reg):
+			print("Registered", user.nama)
 		return user
+	
+	def logout(user):
+		User.logout(user)
+		print("Logged out")
+
+	def get_stasiun():
+		print("Successfully get all stasiun")
+		return Stasiun.get_all()
