820e1a02f97c4c5f62e1c8c50b837228b3ad9ecd
Asyary Raihan <asyaryraihan@gmail.com>
2025-01-03 12:16:38 +0700

    Change db location to appdata

--- main.py
+++ main.py
@@ -1,6 +1,30 @@
 import sys
+import os
+import requests
+from pathlib import Path
 from utils.helper import Utils
 from utils.gui import GUI
+
+# https://raw.githubusercontent.com/asyary/PBO_UAS/refs/heads/master/db.db
+APP_NAME = "Kereta KJT"
+
+appdata_path = os.path.join(os.getenv('APPDATA'), APP_NAME)
+os.makedirs(appdata_path, exist_ok=True) # Just in case dir don't exist
+
+local_db_path = Path(appdata_path) / "db.db"
+db_url = "https://raw.githubusercontent.com/asyary/PBO_UAS/refs/heads/master/db.db"
+
+if not local_db_path.exists():
+    try:
+        print("Downloading database...")
+        response = requests.get(db_url)
+        response.raise_for_status()
+        with open(local_db_path, "wb") as db_file:
+            db_file.write(response.content)
+        print("Database downloaded successfully.")
+    except Exception as e:
+        print(f"Failed to download the database: {e}")
+        exit(1)
 
 def init():
 	if Utils.cek_koneksi():
--- utils/model.py
+++ utils/model.py
@@ -1,20 +1,18 @@
 import sqlite3
 import os
+from pathlib import Path
 
 class DbModel:
-    def __init__(self, db_name=None):
-        if db_name is None:
-            base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-            self.db_name = os.path.join(base_dir, 'db.db')
-        else:
-            self.db_name = db_name
-        self.connection = None
+	def __init__(self):
+		APP_NAME = "Kereta KJT"
+		appdata_path = os.path.join(os.getenv('APPDATA'), APP_NAME)
+		self.db_name = Path(appdata_path) / "db.db"
 
-    def connect(self):
-        self.connection = sqlite3.connect(self.db_name)
-        self.connection.row_factory = sqlite3.Row
-        self.cursor = self.connection.cursor()
+	def connect(self):
+		self.connection = sqlite3.connect(self.db_name)
+		self.connection.row_factory = sqlite3.Row
+		self.cursor = self.connection.cursor()
 
-    def close(self):
-        if self.connection:
-            self.connection.close()
+	def close(self):
+		if self.connection:
+			self.connection.close()
